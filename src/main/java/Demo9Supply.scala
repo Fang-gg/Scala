7b6807121b857b773d540e7653694732c543f24b
184535658@qq.com <1845356586@qq.com>
2021-11-07 13:19:32 +0800

    基本数据类型 选择循环结构

--- src/main/java/Demo9Supply.scala
+++ src/main/java/Demo9Supply.scala
@@ -27,5 +27,67 @@ object Demo9Supply {
     val str : AnyRef = "abc"
 
 
+    // 循环
+    // 计算1~100的和
+    var q = 1
+    var sum = 0
+    while (q <= 100) {
+      sum += q
+      q += 1
+    }
+    println(sum)
+    // do while 循环体至少会被执行一次
+    val x = 100
+    val y = 200
+    var x_y = 0
+    do {
+      x_y = x + y
+    } while (x > y)
+    println(x_y)
+
+    // for 循环
+    // 实际上scala中的for循环同Python类似 只有for each
+    // Range函数
+    // 计算1~100的和
+    val list = new util.ArrayList[Int]()
+    var sum2 = 0
+    for (elem <- Range(1, 101, 1)) {
+      sum2 += elem
+    }
+    println(sum2)
+
+    // 计算1~100的和
+    var sum3 = 0
+    for (i <- 1 to 100) {
+      sum3 += i
+    }
+    println(sum3)
+    // 计算1~100的和
+    var sum4 = 0
+    for (i <- 1 until 101) {
+      sum4 += i
+    }
+    println(sum4)
+    // 计算1~100 奇数的和
+    var sum5 = 0
+    for (i <- 1 until 101 if i % 2 == 1) {
+      sum5 += i
+    }
+    println(sum5)
+
+    // 选择结构
+    val age = 18
+    if (age >= 18) {
+      println("成年")
+    } else if (age < 0) {
+      println("年龄不规范")
+    } else if (age < 18) {
+      println("未成年")
+    }else{
+      println(".....")
+    }
+
+
+
   }
 }
